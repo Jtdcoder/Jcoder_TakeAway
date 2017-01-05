4c02ddfc7b7620b44b7fee8db028b8bdf9fe51b0
Administrator <826439355@qq.com>
2017-01-06 00:42:46 +0800

    <M> 封装Activity基础类BaseActivity

--- app/src/main/java/com/jiatengde/jcoder_takeaway/BaseActivity.java
+++ app/src/main/java/com/jiatengde/jcoder_takeaway/BaseActivity.java
@@ -5,15 +5,16 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
+ * Activity基础类
  * Created by Jcoder on 2017/1/5.
  */
 
-public abstract class BaseActivity extends AppCompatActivity{
+public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
+        setContentView(getLayoutId());
     }
+
+    protected abstract int getLayoutId();
 }
