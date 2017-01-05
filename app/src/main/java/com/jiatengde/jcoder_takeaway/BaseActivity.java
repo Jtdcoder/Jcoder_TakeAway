2368d7a2abf5518cab976066797b5bb86fb2911e
Administrator <826439355@qq.com>
2017-01-05 23:51:10 +0800

    Initial commit <A> 初始化提交

---
+++ app/src/main/java/com/jiatengde/jcoder_takeaway/BaseActivity.java
@@ -0,0 +1,19 @@
+package com.jiatengde.jcoder_takeaway;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jcoder on 2017/1/5.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+}
---
+++ app/src/main/java/com/jiatengde/jcoder_takeaway/MainActivity.java
@@ -0,0 +1,16 @@
+package com.jiatengde.jcoder_takeaway;
+
+import android.os.Bundle;
+
+/**
+ *  作者：Jcoder
+ *  项目：小贾外卖
+ */
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
